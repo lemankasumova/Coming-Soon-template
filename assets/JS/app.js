547e906aae7d80dbf7d1102dc4e42b67325de58c
leman97 <lemankasumova@gmail.com>
2023-07-31 18:21:41 +0400

    end

---
+++ assets/JS/app.js
@@ -0,0 +1,25 @@
+var untilTime = new Date("Dec 31, 2023 00:00:00").getTime();
+
+var x = setInterval(function () {
+    var now = new Date().getTime();
+    var distance = untilTime - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.querySelector("#days").innerHTML = days;
+    document.querySelector("#hours").innerHTML = hours;
+    document.querySelector("#minutes").innerHTML = minutes;
+    document.querySelector("#seconds").innerHTML = seconds;
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.querySelector("#days").innerHTML = "00";
+        document.querySelector("#hours").innerHTML = "00";
+        document.querySelector("#minutes").innerHTML = "00";
+        document.querySelector("#seconds").innerHTML = "00";
+    }
+}, 1000)
+
